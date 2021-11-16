249adaec9c7f9549712366a7ea0f2518440c300e
Diana-nyamai <dnyamai.dn@gmail.com>
2021-11-16 10:32:42 +0300

    update

--- anaconda-clone/src/App.vue
+++ anaconda-clone/src/App.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+  hello
 </div>
 </template>
 
@@ -12,12 +13,9 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
